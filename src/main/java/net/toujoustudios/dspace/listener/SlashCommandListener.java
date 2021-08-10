bbf6bb8d407bfa502928a3b33be2ebd7679984a0
IanToujou <ianbour2016@gmail.com>
2021-08-11 00:17:39 +0200

    Added SlashCommandListener

---
+++ src/main/java/net/toujoustudios/dspace/listener/SlashCommandListener.java
@@ -0,0 +1,24 @@
+package net.toujoustudios.dspace.listener;
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This file has been created by Ian Toujou.
+ * Project: DSpace
+ * Date: 09/08/2021
+ * Time: 12:55
+ */
+public class SlashCommandListener extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommand(@NotNull SlashCommandEvent event) {
+
+        if(event.getGuild() == null) return;
+
+
+
+    }
+
+}
